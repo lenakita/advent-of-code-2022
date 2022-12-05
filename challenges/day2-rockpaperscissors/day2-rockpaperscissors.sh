5161a1927aabf836fe556a1bccb48e3fd87aab23
lenakita <elenafayethomas@gmail.com>
2022-12-05 18:56:41 +0000

    feat(day2): part two completed

--- challenges/day2-rockpaperscissors/day2-rockpaperscissors.sh
+++ challenges/day2-rockpaperscissors/day2-rockpaperscissors.sh
@@ -8,12 +8,20 @@
 # 4. tot up points for round
 # 5. tot up points for all rounds
 
+# ~~~ PART TWO ~~~ #
+# 1. get input for player
+# 2. add to score based on lose, draw or win
+# 3. get opponent input
+# 4. calculate player input based on l/d/w and opponent input
+# 5. use that to calculate the score for picking one of r/p/s
+
 filename="inputs.txt"
-playerinput=""
-opponentinput=""
+player_input=""
+opponent_input=""
 score=0
 
-function compareinputs() {
+# ~~~ PART ONE FUNCTIONS ~~~ #
+function compare_inputs() {
     # $1 is player
     # $2 is opponent
     if [ $1 == $2 ]
@@ -31,7 +39,7 @@ function compareinputs() {
     fi
 }
 
-function getinput() {
+function get_input() {
     local inputarr=()
     local index=0
     local input=""
@@ -62,7 +70,7 @@ function getinput() {
     echo $input
 }
 
-function getinputscore() {
+function get_input_score() {
     # required to add the extra score for the
     # input picked
     if [ $1 == "rock" ]
@@ -77,18 +85,68 @@ function getinputscore() {
     fi
 }
 
+# ~~~ PART TWO FUNCTIONS ~~~ #
+function calculate_input_lose() {
+    # $1 is the opponent's input
+    if [ $1 == "rock" ]
+    then
+        score=$((score + 3))
+    elif [ $1 == "paper" ]
+    then
+        score=$((score + 1))
+    elif [ $1 == "scissors" ]
+    then
+        score=$((score + 2))
+    fi
+}
+
+function calculate_input_win() {
+    # $1 is the opponent's input
+    if [ $1 == "rock" ]
+    then
+        score=$((score + 2))
+    elif [ $1 == "paper" ]
+    then
+        score=$((score + 3))
+    elif [ $1 == "scissors" ]
+    then
+        score=$((score + 1))
+    fi
+}
+
+function get_winning_status() {
+    # $1 is the opponent's input
+    if [ ${splitline[1]} == "X" ]
+    then
+        # lose
+        calculate_input_lose $1
+        score=$((score + 0))
+    elif [ ${splitline[1]} == "Y" ]
+    then
+        # draw
+        get_input_score $1
+        score=$((score + 3))
+    elif [ ${splitline[1]} == "Z" ]
+    then
+        # win
+        calculate_input_win $1
+        score=$((score + 6))
+    fi
+}
+
+
 while read line
 do
     splitline=($line)
     # firstly, get inputs
     # wrapped to capture the `echo`
-    playerinput=$(getinput "player")
-    opponentinput=$(getinput "opponent")
+    # player_input=$(get_input "player")
+    opponent_input=$(get_input "opponent")
     # then the score for picking one of
     # rock, paper or scissors
-    getinputscore $playerinput
+    get_winning_status $opponent_input
     # then the score for winning or losing
-    compareinputs $playerinput $opponentinput
+    # compare_inputs $player_input $opponent_input
 done < $filename
 
 echo "Score: ${score}"
